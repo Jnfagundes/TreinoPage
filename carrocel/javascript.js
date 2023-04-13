1ef0c6d4c4c537f9523fd12be64522fa00e7cfc6
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2023-04-13 16:19:24 -0300

    Criando a animação

--- carrocel/javascript.js
+++ carrocel/javascript.js
@@ -10,10 +10,24 @@ controls.forEach(control => {
         const isLeft = control.classList.contains('.arrow-left');
 
         if (isLeft){
-            currentItem -=1;
+            currentItem -= 1;
         } else {
-            currentItem +=1;
+            currentItem += 1;
         }
-        console.log('control clicked', isLeft);
-});
+
+        if (currentItem >= maxItems ){
+            currentItem = 0;
+        }
+        if (currentItem < 0){
+            currentItem = maxItems - 1;
+        }
+        items.forEach(item => item.classList.remove('current-item'));
+
+        items[currentItem].scrollIntoView({
+            inline: "center",
+            behavior:"smooth",
+        });
+
+        items[currentItem].classList.add("current-item");
+    });
 });

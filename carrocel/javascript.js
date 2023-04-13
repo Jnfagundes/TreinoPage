c602d193b4a16fdba01239a0f9c7f41c0c325248
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2023-04-13 09:17:37 -0300

    Introdução de javascript

---
+++ carrocel/javascript.js
@@ -0,0 +1,19 @@
+const controls = document.querySelectorAll('.control');
+
+let currentItem = 0;
+const items = document.querySelectorAll('.item');
+
+const maxItems = items.length;
+
+controls.forEach(control => {
+    control.addEventListener('click', () => {
+        const isLeft = control.classList.contains('.arrow-left');
+
+        if (isLeft){
+            currentItem -=1;
+        } else {
+            currentItem +=1;
+        }
+        console.log('control clicked', isLeft);
+});
+});
